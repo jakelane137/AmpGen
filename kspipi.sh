495c3d746a77c3da7941b1fcbbe73ae0c2694db8
Jake Lane <john.jake.lane@cern.ch>
2019-08-15 16:32:39 +0100

    added a shell script in bin/ that automatically makes some plots for QC kspipi

---
+++ kspipi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ -z $AMPGENROOT ]
+then
+	echo "Can't find AmpGen installation - the AMPGENROOT variable is not set. Is ampGen installed?"
+else
+	$AMPGENROOT/install/bin/QcGenerator --Output $AMPGENROOT/data/kspipi.root $AMPGENROOT/options/belleQC.opt
+	python3 $AMPGENROOT/python/qcPlot.py --output $AMPGENROOT/data $AMPGENROOT/data/kspipi.root
+fi
+
