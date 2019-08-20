374bee1b7930f872380391d0705aa2c922e129f0
Jake Lane <john.jake.lane@cern.ch>
2019-08-20 13:56:42 +0200

    python change

--- python/qcPlot.py
+++ python/qcPlot.py
@@ -3,7 +3,9 @@ import matplotlib as mpl
 mpl.use('Agg')
 from matplotlib import pyplot as plt
 import numpy as np
-
+mpi = 0.135729
+mK=0.43890
+mD=1.8353
 def mij(pi, pj):
         return (pi[0] + pj[0])**2 - (pi[1] + pj[1])**2 - (pi[2] + pj[2])**2 - (pi[3] + pj[3])**2
 
@@ -18,16 +20,49 @@ def plot1D(obj, name, output):
     fig.savefig(f"{output}")
     plt.close()
 
-def plot2D(obj1, obj2, xname, yname, output):
+def plot2D(obj1, obj2, obj3, proj="XY", xname, yname, output):
     fig = plt.figure(figsize=(10,10))
     nbins = int(len(obj1)/1000)
-    counts, xedges, yedges, im = plt.hist2d(obj1, obj2, nbins, density=True)
+    b = bound(obj1, obj2, obj3)
+    if proj=="XY" or proj=="YX":
+        plt.plot(b[0], b[1])
+        plt.hold()
+        counts, xedges, yedges, im = plt.hist2d(obj1, obj2, nbins)
+    elif proj=="YZ" or proj=="ZY": 
+        plt.plot(b[1], b[2])
+        plt.hold()
+        counts, xedges, yedges, im = plt.hist2d(obj2, obj3, nbins)       
+    elif proj=="ZX" or proj=="XZ":
+        plt.plot(b[2], b[0])
+        plt.hold()
+        counts, xedges, yedges, im = plt.hist2d(obj3, obj1, nbins)       
+    
+
+
     fig.colorbar(im)
     plt.xlabel(f"{xname}")
     plt.ylabel(f"{yname}")
     fig.savefig(f"{output}")
     plt.close()
 
+def bound(obj1, obj2, obj3,N=10000, lim=0.01):
+    X = np.linspace(min(obj1), max(obj1), N)
+    Y = np.linspace(min(obj2), max(obj2), N)
+    Z = np.linspace(min(obj3), max(obj3), N)
+    Xp=[]
+    Yp=[]
+    Zp=[]
+    for x in X:
+        for y in Y:
+            for z in Z:
+                c = x+y+z - mD**2 - mK**2 - mpi**2 - mpi**2
+                if abs(c)>lim:
+                    Xp.append(x)
+                    Yp.append(y)
+                    Zp.append(z)
+    return [Xp,Yp,Zp]
+    
+    
 class qcPlot:
     def __init__(self, file, output, EventType):
         self.file = uproot.open(file)
@@ -62,9 +97,9 @@ class qcPlot:
             plot1D(m12, "$m^2_{12}$", f"{self.output}/{tagName}/m12.png")
             plot1D(m13, "$m^2_{13}$", f"{self.output}/{tagName}/m13.png")
             plot1D(m23, "$m^2_{23}$", f"{self.output}/{tagName}/m23.png")
-            plot2D(m12, m13, "$m^2_{+}$", "$m^2_{-}$", f"{self.output}/{tagName}/m12_vs_m13.png")
-            plot2D(m12, m23, "$m^2_{+}$", "$m^2_{0}$", f"{self.output}/{tagName}/m12_vs_m23.png")
-            plot2D(m13, m23, "$m^2_{-}$", "$m^2_{0}$", f"{self.output}/{tagName}/m13_vs_m23.png")
+            plot2D(m12, m13, m23, "$m^2_{+}$", "$m^2_{-}$", f"{self.output}/{tagName}/m12_vs_m13.png")
+            plot2D(m12, m13, m23, "$m^2_{+}$", "$m^2_{0}$", f"{self.output}/{tagName}/m12_vs_m23.png")
+            plot2D(m12, m13, m23, "$m^2_{-}$", "$m^2_{0}$", f"{self.output}/{tagName}/m13_vs_m23.png")
 
     
 
